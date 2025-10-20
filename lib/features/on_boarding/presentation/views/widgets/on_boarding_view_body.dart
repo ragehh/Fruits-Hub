3e5faf418d3185cbbd07a7190c6c3628421f546d
ragehh <mrageh68@gmail.com>
2025-10-21 01:17:56 +0300

    create CustomButton and add it to OnBoardingView

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -18,6 +20,12 @@ class OnBoardingViewBody extends StatelessWidget {
             activeColor: AppColors.primaryColor,
           ),
         ),
+        const SizedBox(height: 29),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+          child: CustomButton(text: 'ابدأ الان', onPressed: () {}),
+        ),
+        const SizedBox(height: 43),
       ],
     );
   }
