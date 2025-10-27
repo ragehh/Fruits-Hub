c6c7cf3fb16e475a2b25331046faa34af3da0243
ragehh <mrageh68@gmail.com>
2025-10-27 21:10:46 +0300

    Log Errors

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fruits_hub/core/errors/exceptions.dart';
 
@@ -11,16 +13,24 @@ class FirebaseAuthService {
           .createUserWithEmailAndPassword(email: email, password: password);
       return credential.user!;
     } on FirebaseAuthException catch (e) {
+      log(
+        'Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()} and code is ${e.code}',
+      );
       if (e.code == 'weak-password') {
         throw CustomException(message: 'كلمة المرور ضعيفة جدًا.');
       } else if (e.code == 'email-already-in-use') {
         throw CustomException(
           message: 'يوجد حساب بالفعل لهذا البريد الإلكتروني.',
         );
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(message: 'تأكد من اتصالك بالانترنت.');
       } else {
         throw CustomException(message: 'حدث خطأ. يُرجى المحاولة لاحقًا.');
       }
     } catch (e) {
+      log(
+        'Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()}',
+      );
       throw CustomException(message: 'حدث خطأ. يُرجى المحاولة لاحقًا.');
     }
   }
--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
 import 'package:fruits_hub/core/errors/exceptions.dart';
 import 'package:fruits_hub/core/errors/failure.dart';
@@ -26,6 +28,9 @@ class AuthRepoImpl extends AuthRepo {
     } on CustomException catch (e) {
       return left(ServerFailure(e.message));
     } catch (e) {
+      log(
+        'Exception in AuthRepoImpl.createUserWithEmailAndPassword: ${e.toString()}',
+      );
       return left(ServerFailure('حدث خطأ. يُرجى المحاولة لاحقًا.'));
     }
   }
