90e99026f7ecb48b182ba38736a987cadac56a97
ragehh <mrageh68@gmail.com>
2025-11-04 18:10:54 +0200

    Build Best Selling GridView

--- lib/core/widgets/fruit_item.dart
+++ lib/core/widgets/fruit_item.dart
@@ -10,8 +10,6 @@ class FruitItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 163,
-      height: 280,
       decoration: ShapeDecoration(
         color: Color(0xFFF3F5F7),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/constants.dart';
-import 'package:fruits_hub/core/widgets/fruit_item.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/best_selling_grid_view.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/best_selling_header.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/featured_list.dart';
@@ -27,10 +27,10 @@ class HomeViewBody extends StatelessWidget {
                 SizedBox(height: 12),
                 BestSellingHeader(),
                 SizedBox(height: 8),
-                FruitItem(),
               ],
             ),
           ),
+          BestSellingGridView(),
         ],
       ),
     );
