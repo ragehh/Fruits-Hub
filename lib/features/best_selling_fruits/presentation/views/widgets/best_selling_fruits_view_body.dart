e1c19fc3ab85e4c3743c1b51cadb37c73c0c2c8a
ragehh <mrageh68@gmail.com>
2025-11-07 00:16:15 +0200

    Finish Best Selling Fruits View Body UI

--- lib/features/best_selling_fruits/presentation/views/widgets/best_selling_fruits_view_body.dart
+++ lib/features/best_selling_fruits/presentation/views/widgets/best_selling_fruits_view_body.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/best_selling_grid_view.dart';
+
+import '../../../../../core/utils/app_text_styles.dart';
 
 class BestSellingFruitsViewBody extends StatelessWidget {
   const BestSellingFruitsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: const Column(
+              children: [
+                SizedBox(height: 24),
+                Row(
+                  children: [Text('الأكثر مبيعًا', style: TextStyles.bold16)],
+                ),
+                SizedBox(height: 8),
+              ],
+            ),
+          ),
+          BestSellingGridView(),
+        ],
+      ),
+    );
   }
 }
