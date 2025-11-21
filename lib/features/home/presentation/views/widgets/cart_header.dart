2e5f46a714b8fd36e825e234deb161b7698da985
ragehh <mrageh68@gmail.com>
2025-11-21 20:11:44 +0200

    Display Number of Products added in Cart Header

--- lib/features/home/presentation/views/widgets/cart_header.dart
+++ lib/features/home/presentation/views/widgets/cart_header.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
 
 import '../../../../../core/utils/app_text_styles.dart';
+import '../../cubits/cart_cubit/cart_cubit.dart';
 
 class CartHeader extends StatelessWidget {
   const CartHeader({super.key});
@@ -14,7 +16,7 @@ class CartHeader extends StatelessWidget {
       decoration: const BoxDecoration(color: Color(0xFFEBF9F1)),
       child: Center(
         child: Text(
-          'لديك 3 منتجات في سله التسوق',
+          'لديك ${context.watch<CartCubit>().cartEntity.cartItems.length} منتجات في سله التسوق',
           style: TextStyles.regular13.copyWith(color: AppColors.primaryColor),
         ),
       ),
